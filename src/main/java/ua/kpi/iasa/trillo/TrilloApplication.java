b8899ba8e951da3de571e3de76a0536e2c634d69
Makrushyn <a.makrushyn@levi9.com>
2022-09-25 12:32:24 +0300

    Initial commit

---
+++ src/main/java/ua/kpi/iasa/trillo/TrilloApplication.java
@@ -0,0 +1,13 @@
+package ua.kpi.iasa.trillo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrilloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrilloApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/kpi/iasa/trillo/TrilloApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.kpi.iasa.trillo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrilloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
